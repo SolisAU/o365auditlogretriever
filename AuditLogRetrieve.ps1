6c1a3cb3612cc4b229e82027857b2875a1bb410a
David Rudduck <47308254+davidrudduck@users.noreply.github.com>
2021-05-17 17:05:05 +1000

    Massive changes
    Merged in the three different log collectors into one.
    Added parameters `-start`, `-end`, `-logtype` so you dont need to hard code values and can use the one script to do everything.
    Merged in the `Write-Output` and -Write-LogFile` commands to just be `Write-Out` since we dumped everything to StdOut and the log file which seemed redundant coding.
    
    Major change:
    * Made it so when the result size is > 5,000 records it will reduce the interval between the space of time it will query by half and re-run the query. It keeps scaling down till it can collect a batch of results (batch must be 5,000 or less)
    * If we get < 2,500 results it scales the interval back up by double. This is a test to see if we can scale back up quickly when there's no results and cover ground faster.

--- AuditLogRetrieve.ps1
+++ AuditLogRetrieve.ps1
@@ -1,26 +1,70 @@
+# Microsoft Office 365 Audit Log Retriever
+# It's a bit hacky. It scales up and down the interval between queries as necessary.
+# That has a performance hit - but hopefully it's a worthwhile cost.
+
+param (
+  [DateTime] $start,
+  [DateTime] $end,
+  [string] $logtype,
+  [switch] $help
+)
+Write-Output "[X] Microsoft Office 365 Audit Log Retriever"
+Write-Output "    ========================================"
+if($help) {
+	Write-Output "[?] -start		To specify start of log collection (defaults to 90 days from today)"
+	Write-Output "			Use MM/DD/YYYY HH:MM:SS format"
+	Write-Output "[?] -end		To specify end of log collection (defaults to today)"
+	Write-Output "			Use MM/DD/YYYY HH:MM:SS format"
+	Write-Output ""
+	Write-Output "[?] -logtype		To specify Unified, Admin or Mailbox log (defaults to Unified)"
+	Write-Output ""
+	Write-Output "Example: ./AuditLogRetrieve.ps1 -start '02/30/2020 00:00:01' -end '04/15/2020 23:59:59' -logtype Admin"
+	Write-Output ""
+	Write-Output "[?] -help		To see this help"
+	exit
+}
+if(!$start) {
+	[DateTime]$start = (Get-Date).AddDays(-90)
+}
+if(!$end) {
+	[DateTime]$end = Get-Date
+}
+if(!$logtype) {
+	$logtype = 'Unifieid'
+}
+#################################### Configuration Section ###################################################
+$logFile = $logtype + "_progress.log"
+$outputFile = $logtype + "_All.csv"
+
+# Uncomment this if you want the program to prompt you every time
+# Otherwise just run these ONCE before you start your investigation
+
 #Install-Module -Name ExchangeOnlineManagement
 #Connect-ExchangeOnline
 
-#################################### Configuration Section ###################################################
-$logFile = "UAL_All.log"
-$outputFile = "UnifiedAuditLog_All.csv"
-[DateTime]$start = (Get-Date).AddDays(-90)
-#[DateTime]$start = '07/09/2020 03:16:13'
-[DateTime]$end = Get-Date
+# Manually set the start and end
+#[DateTime]$start = '05/16/2020 00:00:00'
+#[DateTime]$end = '06/19/2020 23:59:59'
 $resultSize = 1000
-$intervalMinutes = 6
+$defaultintervalMinutes = 60
 $retryCount = 3
-Write-Output "[***] Retrieving Office 365 logs from $($start) till $($end) [***]"
+Write-Output "[***] Retrieving Microsoft 365 $logtype audit logs from $($start) till $($end) [***]"
+Write-Output "[?] Outputing to $outputfile"
+
 #################################### End Configuration Section ###################################################
+
 [DateTime]$currentStart = $start
 [DateTime]$currentEnd = $start
 $currentTries = 0
+$intervalMinutes = $defaultintervalMinutes
+$originalEnd = $currentEnd
 
- 
-Function Write-LogFile ([String]$Message)
+Function Write-Out ([String]$Message)
 {
+	# Padded this out to merge Logfile output and Screen output to save text
     $final = [DateTime]::Now.ToString() + ":" + $Message
     $final | Out-File $logFile -Append
+	Write-Output $Message
 }
  
 while ($true)
@@ -32,46 +76,62 @@ while ($true)
     }
     $currentTries = 0
     $sessionID = [DateTime]::Now.ToString().Replace('/', '_')
-    Write-LogFile "INFO: Retrieving audit logs between $($currentStart) and $($currentEnd)"
-    Write-Output "INFO: Retrieving audit logs between $($currentStart) and $($currentEnd)"
+    Write-Out "INFO: Retrieving $logtype logs between $($currentStart) and $($currentEnd) [$intervalMinutes mins]"
     $currentCount = 0
     while ($true)
     {
-        [Array]$results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnNextPreviewPage -ResultSize $resultSize
+        if($logtype -eq "Unified") {
+			[Array]$results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnNextPreviewPage -ResultSize $resultSize
+		}
+		if($logtype -eq "Admin") {
+			[Array]$results = Search-AdminAuditLog -StartDate $currentStart -EndDate $currentEnd -ResultSize $resultSize
+		}
+		if($logtype -eq "Mailbox") {
+			[Array]$results = Search-MailboxAuditLog -StartDate $currentStart -EndDate $currentEnd -ShowDetails -IncludeInactiveMailbox -ResultSize $resultSize
+		}
         if ($results -eq $null -or $results.Count -eq 0)
         {
             #Retry if needed. This may be due to a temporary network glitch
             if ($currentTries -lt $retryCount)
             {
                 $currentTries = $currentTries + 1
-                #Start-Sleep -Milliseconds 5
+                Start-Sleep -Milliseconds 5
                 continue
             }
             else
-            {
-                Write-LogFile "WARNING: Empty data set returned between $($currentStart) and $($currentEnd). Retry count reached. Moving forward!"
-                Write-Output "WARNING: Empty data set returned between $($currentStart) and $($currentEnd). Retry count reached. Moving forward!"
+            {             
+				#if ($intervalMinutes -lt 60 -and $results.Count -lt 2500)
+				if ($results.Count -lt 2500)
+				{
+					$intervalMinutes = $intervalMinutes * 2
+				}
+				Write-Out "WARNING: Empty data set returned between $($currentStart) and $($currentEnd). Retry count reached. Moving forward with interval of $intervalMinutes minutes!"
                 break
             }
         }
         $currentTotal = $results[0].ResultCount
         if ($currentTotal -gt 5000)
         {
-            Write-LogFile "WARNING: $($currentTotal) total records match the search criteria. Some records may get missed. Consider reducing the time interval!"
-            Write-Output "WARNING: $($currentTotal) total records match the search criteria. Some records may get missed. Consider reducing the time interval!"
+            Write-Out "WARNING: $($currentTotal) total records match the search criteria."
+            $intervalMinutes = $intervalMinutes / 2
+            $originalEnd = $currentEnd
+            Write-Out "Attempting $currentStart to $currentEnd with smaller batch of $intervalMinutes minutes"
+            break
         }
         $currentCount = $currentCount + $results.Count
-        Write-LogFile "INFO: Retrieved $($currentCount) records out of the total $($currentTotal)"
-        Write-Output "INFO: Retrieved $($currentCount) records out of the total $($currentTotal)"
-        $results | epcsv $outputFile -NoTypeInformation -Append
+        Write-Out "INFO: Retrieved $($currentCount) records out of the total $($currentTotal)"
+        $results | Export-CSV $outputFile -NoTypeInformation -Append
         if ($currentTotal -eq $results[$results.Count - 1].ResultIndex)
         {
             $message = "INFO: Successfully retrieved $($currentTotal) records for the current time range. Moving on!"
-            Write-LogFile $message
-            Write-Output $message
+            Write-Out $message
+            if ($currentEnd -eq $originalEnd)
+            {
+                $intervalMinutes = $defaultintervalMinutes
+            }
             break
         }
     }
-    $currentStart = $currentEnd
+	$currentStart = $currentEnd
 }
 Remove-PSSession $Session
